adf9a6e07cc8092733f0ab6803b8a9ba3c9b89d0
kornect <mogauisakm@live.com>
2023-03-15 12:46:02 +0200

    Added global shared utils library

---
+++ libs/shared/utils/src/lib/shared-utils.spec.ts
@@ -0,0 +1,157 @@
+import {
+  getExpirationDate,
+  hasDuplicates,
+  isNullOrEmptyArray,
+  isNullOrUndefined,
+  objectToValuesArray,
+  paramsToDictionary,
+  removeNulls,
+  replaceNonAlphaNumeric,
+} from './shared-utils';
+
+describe('shared-utils', () => {
+  describe('objectToValuesArray', () => {
+    it('should convert object to values array', () => {
+      const expected = [1, 2, 3];
+      const notExpected = ['1', '1', '1', '1'];
+      const actual = objectToValuesArray({ a: 1, b: 2, c: 3 });
+
+      expect(actual).toEqual(expected);
+      expect(actual).not.toEqual(notExpected);
+    });
+
+    it('should convert object to values array', () => {
+      const expected: any[] = [];
+      const actual = objectToValuesArray({});
+
+      expect(actual).toEqual(expected);
+    });
+  });
+
+  describe('replaceNonAlphaNumeric', () => {
+    it('should replace non alphanumeric characters', () => {
+      const expected = 'a_b_c';
+      const actual = replaceNonAlphaNumeric('a b c');
+
+      expect(actual).toEqual(expected);
+    });
+    it('should replace non alphanumeric characters', () => {
+      const expected = 'abc';
+      const actual = replaceNonAlphaNumeric('abc');
+
+      expect(actual).toEqual(expected);
+    });
+  });
+
+  describe('isNullOrEmptyArray', () => {
+    it('should check if array is null', () => {
+      const expected = true;
+      const actual = isNullOrEmptyArray(null);
+
+      expect(actual).toEqual(expected);
+    });
+
+    it('should check if array is undefined', () => {
+      const expected = true;
+      const actual = isNullOrEmptyArray(undefined);
+
+      expect(actual).toEqual(expected);
+    });
+
+    it('should check if array is empty', () => {
+      const expected = true;
+      const actual = isNullOrEmptyArray([]);
+
+      expect(actual).toEqual(expected);
+    });
+
+    it('should check if array is not empty', () => {
+      const expected = false;
+      const actual = isNullOrEmptyArray([1, 2]);
+
+      expect(actual).toEqual(expected);
+    });
+  });
+
+  describe('isNullOrUndefined', () => {
+    it('should check if value is null', () => {
+      const expected = true;
+      const actual = isNullOrUndefined(null);
+
+      expect(actual).toEqual(expected);
+    });
+
+    it('should check if value is undefined', () => {
+      const expected = true;
+      const actual = isNullOrUndefined(undefined);
+
+      expect(actual).toEqual(expected);
+    });
+
+    it('should check if value is undefined', () => {
+      const expected = false;
+      const actual = isNullOrUndefined({ a: 1, b: 2, c: 3 });
+
+      expect(actual).toEqual(expected);
+    });
+  });
+
+  describe('paramsToDictionary', () => {
+    it('should convert params to dictionary', () => {
+      const expected = { a: '1', b: '2', c: '3' };
+      const actual = paramsToDictionary([
+        { key: 'a', value: '1' },
+        { key: 'b', value: '2' },
+        { key: 'c', value: '3' },
+      ]);
+
+      expect(actual).toEqual(expected);
+    });
+  });
+
+  describe('hasDuplicates', () => {
+    it('should check if array has duplicates', () => {
+      const expected = true;
+      const actual = hasDuplicates([1, 2, 3, 1]);
+
+      expect(actual).toEqual(expected);
+    });
+
+    it('should check if array has no duplicates', () => {
+      const expected = false;
+      const actual = hasDuplicates([1, 2, 3]);
+
+      expect(actual).toEqual(expected);
+    });
+  });
+
+  describe('removeNulls', () => {
+    it('should remove nulls from object', () => {
+      const expected = { a: 1, c: 3 };
+      const actual = removeNulls({ a: 1, b: null, c: 3 });
+
+      expect(actual).toEqual(expected);
+    });
+  });
+
+  describe('getExpirationDate', () => {
+    it('should return expiration date', () => {
+      const expected = new Date(2020, 1, 1, 3, 0, 0);
+      const actual = getExpirationDate('3h', new Date(2020, 1, 1));
+
+      expect(actual).toEqual(expected);
+    });
+    it('should return expiration date', () => {
+      const expected = new Date(2020, 1, 1, 0, 3, 0);
+      const actual = getExpirationDate('3m', new Date(2020, 1, 1));
+
+      expect(actual).toEqual(expected);
+    });
+    it('should throw invalid error', () => {
+      expect(() => getExpirationDate('3', new Date(2020, 1, 1))).toThrowError('Invalid expiration format, format must have time post fix (s, m, h, d, ms). Example: 1d, 1h, 1m, 1s, 1ms');
+    });
+    it('should throw expiresIn required error', () => {
+      expect(() => getExpirationDate('', new Date(2020, 1, 1))).toThrowError('Expiration format is required');
+    });
+  });
+});
---
+++ libs/shared/utils/src/lib/shared-utils.ts
@@ -0,0 +1,103 @@
+import { addDays, addHours, addMilliseconds, addMinutes, addSeconds } from 'date-fns';
+
+export function objectToValuesArray(obj: any): any[] {
+  const values: any[] = [];
+  Object.keys(obj).forEach((key) => {
+    values.push(obj[key]);
+  });
+
+  return values;
+}
+
+export function replaceNonAlphaNumeric(str: string): string {
+  return str.replace(/[^a-zA-Z0-9]/g, '_');
+}
+
+// checks if the given value is a string, not null or undefined
+export function isNullOrEmpty(data: string) {
+  return data === null || data === undefined || data === '';
+}
+
+export function isNullOrWhitespace(data: string) {
+  return data === null || data === undefined || data.trim() === '';
+}
+
+export function isNullOrUndefined(data: any) {
+  return data === null || data === undefined;
+}
+
+export function paramsToDictionary(
+  params: {
+    key: string;
+    value: string;
+  }[]
+): { [key: string]: string } {
+  const dictionary: { [key: string]: string } = {};
+  params.forEach((param) => {
+    dictionary[param.key] = param.value;
+  });
+  return dictionary;
+}
+
+export function isProduction(): boolean {
+  return process.env['NODE_ENV'] === 'production';
+}
+
+export function isNullOrEmptyArray(value: any[]) {
+  return value === null || value === undefined || (value as any[])?.length === 0;
+}
+
+export function hasDuplicates(array: any[]) {
+  // check if array is empty or has only one element
+  if (isNullOrEmptyArray(array) || array.length < 2) {
+    return false;
+  }
+  return new Set(array).size !== array.length;
+}
+
+export function dateOrNull(value: string | Date | null | undefined) {
+  if (!value) {
+    return null;
+  }
+  return new Date(value);
+}
+
+export function removeNulls(obj: any) {
+  Object.keys(obj).forEach((key) => {
+    if (obj[key] && typeof obj[key] === 'object') removeNulls(obj[key]);
+    else if (isNullOrEmpty(obj[key])) delete obj[key];
+  });
+  return obj;
+}
+
+export function isEmpty(obj: any) {
+  if (isNullOrUndefined(obj)) {
+    return true;
+  }
+
+  const newObj = removeNulls(obj);
+
+  return Object.keys(newObj).length === 0;
+}
+
+export function getExpirationDate(expirationFormat: string, referenceDate?: Date) {
+  if (isNullOrEmpty(expirationFormat)) {
+    throw new Error('Expiration format is required');
+  }
+  const now = referenceDate ?? new Date();
+
+  // check if expirationFormat is in seconds or milliseconds or minutes or hours or days into milliseconds
+  if (expirationFormat.endsWith('s')) {
+    return addSeconds(now, parseInt(expirationFormat.replace('s', '')));
+  } else if (expirationFormat.endsWith('m')) {
+    return addMinutes(now, parseInt(expirationFormat.replace('m', '')));
+  } else if (expirationFormat.endsWith('h')) {
+    return addHours(now, parseInt(expirationFormat.replace('h', '')));
+  } else if (expirationFormat.endsWith('d')) {
+    return addDays(now, parseInt(expirationFormat.replace('d', '')));
+  } else if (expirationFormat.endsWith('ms')) {
+    return addMilliseconds(now, parseInt(expirationFormat.replace('ms', '')));
+  } else {
+    throw new Error('Invalid expiration format, format must have time post fix (s, m, h, d, ms). Example: 1d, 1h, 1m, 1s, 1ms');
+  }
+}
