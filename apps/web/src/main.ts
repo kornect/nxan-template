05e5cb7a2c682a11a999c74f6aa6f46957938611
kornect <mogauisakm@live.com>
2023-03-27 21:49:46 +0200

    Added proxy.config.json

--- apps/web/src/main.ts
+++ apps/web/src/main.ts
@@ -3,7 +3,21 @@ import { provideRouter, withEnabledBlockingInitialNavigation } from '@angular/ro
 
 import { AppComponent } from './app/app.component';
 import { appRoutes } from './app/app.routes';
+import { ApiModule } from '@nxan/clients/api-client';
+import { AuthModule } from '@nxan/clients/auth-client';
+import { importProvidersFrom } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 bootstrapApplication(AppComponent, {
-  providers: [provideRouter(appRoutes, withEnabledBlockingInitialNavigation())],
+  providers: [
+    importProvidersFrom(
+      HttpClientModule,
+      ApiModule.forRoot({ rootUrl: 'http://localhost:3333' }),
+      AuthModule.forRoot({
+        homeUrl: '/',
+        loginUrl: '/login',
+      })
+    ),
+    provideRouter(appRoutes, withEnabledBlockingInitialNavigation())
+  ],
 }).catch((err) => console.error(err));
